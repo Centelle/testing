c5ffacbedb8f5ce59d57ccc5dc55dd96f88bb980
Arin Roday <arinsroday@gmail.com>
2020-11-24 12:18:39 +0530

    initial commit #1

---
+++ A/build.sh
@@ -0,0 +1,1 @@
+g++ -std=c++14 -Wshadow -Wall -o A.exe A.cpp -g -fsanitize=address -fsanitize=undefined -Wextra -O2 -pedantic -Wformat=2 -Wfloat-equal -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -DLOCAL
